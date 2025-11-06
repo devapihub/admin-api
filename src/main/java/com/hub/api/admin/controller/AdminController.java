bab3eb47f4824b5d8b7c090ab0ea32c09a12983f
hugh.huynh <trihuynh030697@gmail.com>
2025-11-06 18:18:40 +0700

    update build ci/cd build hihi

--- src/main/java/com/hub/api/admin/controller/AdminController.java
+++ src/main/java/com/hub/api/admin/controller/AdminController.java
@@ -13,7 +13,7 @@ import java.util.Map;
 public class AdminController {
     @GetMapping
     public Object hello() {
-        return Map.of("status", "OK", "message", "Complete CI/CD for admin service");
+        return Map.of("status", "OK", "message", "Successfully accessed admin service");
     }
 
     @GetMapping("user")
